2b34b91d4a8e4e4e918f526ee1673dcb104c452f
Margarida Martelo <margmartelo@gmail.com>
2020-10-21 15:02:09 +0100

    Adding methods in order to make our program easier to read

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# Now let's print them
-puts "The students of Villians Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villians Academy"
+  puts "-------------"
 end
-# Printing the total number of students
-puts "Overall, we have #{students.count} great students"
+# Now let's print them
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  # Printing the total number of students
+  puts "Overall, we have #{names.count} great students"
+end
+#Until we call the methods, nothing will happen
+print_header
+print(students)
+print_footer(students)
