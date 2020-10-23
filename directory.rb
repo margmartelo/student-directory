4c976c2ea5a585d6cf636db898fe9845dfee57bf
Margarida Martelo <margmartelo@gmail.com>
2020-10-23 17:32:53 +0100

    Refactoring the code

--- directory.rb
+++ directory.rb
@@ -17,20 +17,18 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
+  @students = []
   name = gets.chomp
   # while the name is not empty (if it isn't empty, 'name.empty?' will evaluate
   # false, so '!name.empty' will evaluate true and continue the loop), repeat
   # this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-
-  students
 end
 
 def print_header
@@ -38,43 +36,55 @@ def print_header
   puts "-------------"
 end
 # Now let's print them
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
+def print_footer
   # Printing the total number of students
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu(students)
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+# def interactive_menu
+#   students = []
 
-    answer = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-    case answer
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+    case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit #this will cause the program to terminate
     else
       puts "I don't know what you meant,try again"
     end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
-students = input_students
+# students = input_students
 #Until we call the methods, nothing will happen
 # print_header
 # print(students)
 # print_footer(students)
-interactive_menu(students)
+interactive_menu
